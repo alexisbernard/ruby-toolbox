02d9d73037c47a82c31cee7505de17416d2e7be9
Alexis Bernard <alexis@bernard.io>
2014-03-20 22:01:38 +0100

    Form builder is more Rails way.

--- form_builder.rb
+++ form_builder.rb
@@ -3,17 +3,17 @@
 # Happens error message in label field name. It's an extension form builder label method.
 # Usage:
 #
-#   <%= form_for @user do |form| %>
+#   <%= form_for @user, builder: InlineErrorFormBuilder do |form| %>
 #     <%= form.label_with_error :email %>
 #     <%= form.email_field :email %>
 #     ...
 #
-class ActionView::Helpers::FormBuilder
-  def label_with_error(field, text = object.class.human_attribute_name(field))
+class InlineErrorFormBuilder < ActionView::Helpers::FormBuilder
+  def label(field, text = object.class.human_attribute_name(field))
     if error = object.errors[field].first
-      label(field, text + ' ' + error)
+      super(field, text + ' ' + error)
     else
-      label(field, text)
+      super(field, text)
     end
   end
 end

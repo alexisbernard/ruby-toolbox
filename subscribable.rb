a1b8b5d742ec4ad03eac836357264e59f7228b72
Alexis Bernard <alexis@bernard.io>
2014-03-21 14:49:09 +0100

    Better name.

--- subscribable.rb
+++ subscribable.rb
@@ -1,26 +1,31 @@
 module Subscribable
-  def subscribed_callbacks
-    @subscribed_callbacks ||= {}
+  def subscribed_blocks
+    @subscribed_blocks ||= {}
   end
 
   def subscribe(name, method = nil, &block)
-    (subscribed_callbacks[name] ||= []) << (method || block)
+    (subscribed_blocks[name] ||= []) << (method || block)
   end
 
   def publish(name, *args)
-    subscribed_callbacks[name].each { |block| block.call(*args) }
+    blocks = subscribed_blocks[name] and blocks.each { |block| block.call(*args) }
   end
 end
 
 if $PROGRAM_NAME == __FILE__
-  puts string = "This test must print 2 lines if it passes."
+  $call_count = 0
+  string = "This test must print 2 lines if it passes."
   string.extend(Subscribable)
+  string.publish(:foo, "This text should not be printed.")
 
   def bar(arg)
     puts arg
+    $call_count += 1
   end
 
-  string.subscribe(:foo) { |arg| puts arg }
+  string.subscribe(:foo) { |arg| puts arg; $call_count += 1 }
   string.subscribe(:foo, method(:bar))
-  string.publish(:foo, "Test")
+  string.publish(:foo, "This text must be printed 2 times.")
+
+  raise "Fail" if $call_count != 2
 end

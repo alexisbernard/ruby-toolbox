986a2fa28622b6a187dfb8f961f5396f04410785
Alexis Bernard <alexis@bernard.io>
2014-04-19 09:27:48 +0200

    Refactor RandomString in a class.

--- random_string.rb
+++ random_string.rb
@@ -1,12 +1,24 @@
-module RandomString
+class RandomString
   DEFAULT_LENGTH = 16
   DEFAULT_CHARS = (('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a).freeze
 
+  attr_reader :length, :chars
+
   def self.roll(length = DEFAULT_LENGTH, chars = DEFAULT_CHARS)
-    if block_given?
-      loop { yield(token = roll(chars, length)) or return token }
-    else
-      (1..length).reduce('') { |str, i| str << chars[rand(chars.length)] }
-    end
+    new(length, chars).roll
   end
+
+  def initialize(length = DEFAULT_LENGTH, chars = DEFAULT_CHARS)
+    @length = length
+    @chars = chars
+  end
+
+  def roll
+    (1..length).reduce('') { |str, i| str << chars[rand(chars.length)] }
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  raise "16" if RandomString.roll.size != 16
+  raise "Different" if RandomString.roll == RandomString.roll
 end
